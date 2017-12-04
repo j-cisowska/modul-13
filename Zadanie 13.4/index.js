7f47d7c6edea4b1cf86626fb199ead888c437e82
unknown <asiac00@gmail.com>
2017-12-04 12:47:35 +0100

    task 4 fix

--- Zadanie 13.4/index.js
+++ Zadanie 13.4/index.js
@@ -1,50 +1,20 @@
 var fs = require('fs');
 
-var getFiles = function() {
-fs.readdir('./Zadanie 13.4', 'utf-8', function(err, files) {
-    if (files.length) {
-		fs.writeFile('info/copy.txt', files, function(err) {
-        if (err) throw err;
-        console.log('Zapisano!');
-        fs.readFile('info/copy.txt', 'utf-8', function(err, data) {
-            console.log('Nowy plik')
-            });
-		});
-	});
-	else { 
-	return console.log(files);   
-		};
-}
-/*fs.writeFile('info/copy.txt', files, function(err) {
-        if (err) throw err;
-        console.log('Zapisano!');
-        fs.readFile('info/copy.txt', 'utf-8', function(err, data) {
-            console.log('Nowy plik')
-            console.log(data);
-        });
-    }); */
-	
-	
-	//console.log('Zawartość folderu!');
-	
-	
-	/*var fs = require('fs');
-
- 
  fs.readdir(__dirname, 'utf-8', function(err, files) {
     if (err) return;
+	console.log('Zawartość folderu!');
 	files.forEach(function(f) {
         console.log('File: ' + f);
     });
 
-	 fs.writeFile('./info/third.js', files.toString(), function(err) {
+	 fs.writeFile('./info/zawartosc-katalogu.txt', files.toString(), function(err) {
         if (err) throw err;
-        console.log('Zapisano!');
-        fs.readFile('./info/third.js', 'utf-8', function(err, data) {
-            console.log('Dane po zapisie')
+        console.log('Zapisano plik txt z zawartością folderu!');
+        fs.readFile('./info/zawartosc-katalogu.txt', 'utf-8', function(err, data) {
+            console.log('Takie pliki są w folderze')
             console.log(data);
         });
     });
 	
 
-}); */
+});
