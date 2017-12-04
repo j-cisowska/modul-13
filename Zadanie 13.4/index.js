51e1a40ea91d83a800a30690f62c63937b91a787
unknown <asiac00@gmail.com>
2017-12-04 11:55:34 +0100

    another fix of task

--- Zadanie 13.4/index.js
+++ Zadanie 13.4/index.js
@@ -1,27 +1,50 @@
 var fs = require('fs');
 
-fs.readFile('./index.js', 'utf-8', function(err, data) {
-    console.log('Plik index.js');
-    console.log(data);
-    fs.writeFile('info/index.js', data, function(err) {
+var getFiles = function() {
+fs.readdir('./Zadanie 13.4', 'utf-8', function(err, files) {
+    if (files.length) {
+		fs.writeFile('info/copy.txt', files, function(err) {
         if (err) throw err;
         console.log('Zapisano!');
-        fs.readFile('info/index.js', 'utf-8', function(err, data) {
-            console.log('Skopiowany plik index.js!')
+        fs.readFile('info/copy.txt', 'utf-8', function(err, data) {
+            console.log('Nowy plik')
+            });
+		});
+	});
+	else { 
+	return console.log(files);   
+		};
+}
+/*fs.writeFile('info/copy.txt', files, function(err) {
+        if (err) throw err;
+        console.log('Zapisano!');
+        fs.readFile('info/copy.txt', 'utf-8', function(err, data) {
+            console.log('Nowy plik')
             console.log(data);
         });
-    });
-});
+    }); */
+	
+	
+	//console.log('Zawartość folderu!');
+	
+	
+	/*var fs = require('fs');
 
-fs.readFile('./package.json', 'utf-8', function(err, data) {
-    console.log('Plik package.json');
-    console.log(data);
-    fs.writeFile('info/package.json', data, function(err) {
+ 
+ fs.readdir(__dirname, 'utf-8', function(err, files) {
+    if (err) return;
+	files.forEach(function(f) {
+        console.log('File: ' + f);
+    });
+
+	 fs.writeFile('./info/third.js', files.toString(), function(err) {
         if (err) throw err;
         console.log('Zapisano!');
-        fs.readFile('info/package.json', 'utf-8', function(err, data) {
-            console.log('Skopiowany plik package.json!')
+        fs.readFile('./info/third.js', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie')
             console.log(data);
         });
     });
-});
+	
+
+}); */
