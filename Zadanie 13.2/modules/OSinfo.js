a0c8f5b7f0b0ae8656983d0c75a9d78e2b0dda07
unknown <asiac00@gmail.com>
2017-12-06 16:36:14 +0100

    fix time

--- Zadanie 13.2/modules/OSinfo.js
+++ Zadanie 13.2/modules/OSinfo.js
@@ -14,7 +14,7 @@ function getOSinfo() {
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', Math.floor(uptime / 3600).toFixed(0), 'hrs', Math.floor(uptime % 3600 / 60).toFixed(0), 'min', Math.floor(uptime % 3600 % 60).toFixed(0), 'sec');
+    console.log('Uptime: ~', Math.floor(uptime / 3600), 'hrs', Math.floor(uptime % 3600 / 60), 'min', Math.floor(uptime % 3600 % 60), 'sec');
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
